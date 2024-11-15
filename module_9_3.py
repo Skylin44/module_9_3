124480c2bd5da621c7357ceca23677958e5b05b7
Елена Владимировна <50554917+Skylin44@users.noreply.github.com>
2024-11-15 12:43:20 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,11 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = ([len(x) - len(y)] for x, y in zip(first, second) if len(x) != len(y))
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+
+for i in first_result:
+     print(i, end = ', ')
+
+print(list(first_result))
+print(list(second_result))
